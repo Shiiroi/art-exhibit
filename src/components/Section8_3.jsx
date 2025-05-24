ef418fbf0af19e3b22db1d0ee9139922ef8fde6f
Shiiroi <vmagwili@gmail.com>
2025-05-24 22:20:09 +0800

    e

--- src/components/Section8_3.jsx
+++ src/components/Section8_3.jsx
@@ -20,7 +20,7 @@ const Section8_3 = React.forwardRef((props, ref) => (
           Demand Accountability
         </h1>
         <p className="text-[1.15rem] leading-[1.7] opacity-90 text-justify drop-shadow-[0_1px_4px_rgba(0,0,0,0.3)]">
-        An artwork by @allweknowiscj on X. Symbolizes on how th government is in a comfortable position while
+        An artwork by @allweknowiscj on X. Symbolizes on how the government is in a comfortable position while
         the its people suffer from the consequences of their incompetence. Demanding accountability and refuses to glorify resiliency. 
         </p>
       </div>
